70171366663b51f428233e8e63720e10887db73b
Dante <ajsantos.082@gmail.com>
2024-06-28 11:04:02 +0200

    feat: D102 added form data validation

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,13 +1,42 @@
-let accept = document.getElementById('accept')
-let send = document.getElementById('send')
+const user = document.getElementById('user')
+const pass = document.getElementById('pass')
+const accept = document.getElementById('accept')
+const send = document.getElementById('send')
 
 function changeSubmit() {
-    if (send.hasAttribute('disabled')) {
-        send.removeAttribute('disabled');
-    } 
-    else {
-        send.setAttribute('disabled', 'disabled');
+    let send = document.getElementById('send')
+    if (send.classList.contains('disabled')) {
+        send.classList.replace('disabled', 'enabled')
+    } else {
+        send.classList.replace('enabled', 'disabled')
     }
 }
 
+function alertLogin() {
+    const params = new URLSearchParams(location.search)
+    if (params.has('login')) {
+        alert('Vuelve a intentarlo\nLos datos introducidos son incorrectos')
+    }
+}
+
+function testForm(par1, par2) {
+    let pass = true
+    if (par1.length < 1 || par2.length < 1) {
+        alert('Revisa todos los campos, te faltan datos.')
+        pass = false
+        return
+    }
+    if (par1.toString().split(' ').length > 1 || par2.toString().split(' ').length > 1) {
+        alert('Los espacios no están permitidos')
+        pass = false
+        return
+    }
+    if (pass = true) {
+        window.open('../html/auth.html?user=' + par1 + '&pass=' + par2, '_self')
+    }
+}
+
+setTimeout(alertLogin, 500)
+
 accept.addEventListener("click", changeSubmit)
+send.addEventListener("click", () => testForm(user.value, pass.value))
