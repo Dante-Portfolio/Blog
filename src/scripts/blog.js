d65da4cda62816e9e0b91d925caf6573d05a010b
Dante <ajsantos.082@gmail.com>
2024-06-30 11:04:41 +0200

    feat: D403.01 Added blog filter function

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -1,21 +1,13 @@
 const userBox = document.getElementById('userBox')
 const data = new URLSearchParams(location.search)
-const user = data.get('user')
-const filters = document.getElementsByName('filter')
-const mainContainer = document.getElementById('mainContainer')
+const mainContainer = document.getElementById('sectionTitle')
+const filter = document.getElementsByName('filter')
+const articlesBox = document.getElementById('articlesBox')
+const codeForm = document.getElementById('codeForm')
+const levelForm = document.getElementById('levelForm')
 
-// FILTRADO E INNER
-userBox.innerHTML = user
-
-function selectFilter(par) {
-    let filterTittle = filters[par].nextElementSibling.innerHTML
-    mainContainer.innerHTML = filterTittle
-}
-
-for (let i = 0; i < filters.length; i++) {
-    filters[i].addEventListener('click', function () { selectFilter(i) })
-}
-// ...................................................................
+codeForm.reset()
+levelForm.reset()
 
 // FETCH & ACCESS TO DATA
 async function getArticles() {
@@ -24,12 +16,63 @@ async function getArticles() {
     return articlesJson
 }
 
-async function getObjects(par1, par2) {
+let dataList = []
+async function getObjects() {
+    dataList = []
     const data = await getArticles()
-    console.log(data[par1])
-    console.log(data[par1][par2])
+    for (const item of data) {
+        dataList.push(item)
+    }
+}
+// ...................................................................
+
+// TITLE INNER
+
+function innerTitle(par) {
+    let filterTittle = par.nextElementSibling.innerHTML
+    mainContainer.innerHTML = filterTittle
+}
+// ....................................................................
+
+// FILTER
+let codeFilter = []
+let levelFilter = []
+
+function select() {
+    codeFilter = []
+    levelFilter = []
+    const filterSelect = document.querySelectorAll('input[name="filter"]:checked')
+
+    codeSelect = filterSelect[0]
+    levelSelect = filterSelect[1]
+    innerTitle(codeSelect)
+
+    for (let item of dataList)
+        if (item.labels[0] === codeSelect.nextElementSibling.innerHTML) {
+            codeFilter.push(item)
+        }
+
+    if (codeFilter.length === 0) {
+        codeFilter = dataList
+    }
+
+    for (let item of codeFilter)
+        if (item.labels[1] === levelSelect.nextElementSibling.innerHTML) {
+            levelFilter.push(item)
+        }
+
+    if (levelFilter.length === 0) {
+        levelFilter = codeFilter
+    }
+
+    console.log('------------------------')
+    console.log(codeSelect.nextElementSibling.innerHTML + ' ' + codeFilter.length)
+    console.log(levelSelect.nextElementSibling.innerHTML + ' ' + levelFilter.length)
+}
+// ...................................................................
+
+for (let i = 0; i < filter.length; i++) {
+    filter[i].addEventListener('click', function () { select() })
 }
 
-getObjects(0, 'image')
-getObjects(4, 'author')
-// ...................................................................
+getObjects()
